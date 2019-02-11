a49293edb8939db77db5b1bbf27edc89a9c7d728
alexander "sandy" gilfillan <alexander.l.gilfillan@gmail.com>
2019-02-10 21:05:47 -0800

    add styled components, restructure some root frontend level things

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 
-class App extends Component {
+export class App extends Component {
   render() {
     return (
       <div>
-          Hello World
+        Hi there
       </div>
     );
   }
 }
-
-export default App;
