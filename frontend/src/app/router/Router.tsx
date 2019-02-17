65f26ad55931bef7f8570606affdc567c46ca59b
alexander "sandy" gilfillan <alexander.l.gilfillan@gmail.com>
2019-02-16 18:38:34 -0800

    server runs and sees incoming request

--- frontend/src/app/router/Router.tsx
+++ frontend/src/app/router/Router.tsx
@@ -2,11 +2,37 @@ import * as React from "react";
 // import { connect } from "react-redux";
 import { styled } from "../../style/styled-components";
 
-export default class Router extends React.Component {
+
+async function logFetch(url:string) {
+  try {
+    const response = await fetch(url);
+    console.log(await response.text());
+  }
+  catch (err) {
+    console.log('fetch failed', err);
+  }
+}
+
+export default class Router extends React.Component<{}> {
+
+  constructor(props:{}) {
+    super(props);
+  }
+
+  componentWillMount(): void {
+    logFetch('')
+  }
+
+  onCallApi = () => {
+    // logFetch('https://localhost:9000');
+    var exampleSocket = new WebSocket("ws://localhost:9000");
+    exampleSocket.send("Here's some text that the server is urgently awaiting!");
+  };
+
   render() {
     return (
       <RouterWrap>
-        Hi there
+        <button onClick={this.onCallApi}>Call API</button>
       </RouterWrap>
     );
   }
